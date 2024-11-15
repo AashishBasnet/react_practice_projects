ebab5f02190dc8fcfe995e4d659881dc99c877e6
AashishBasnet <basnetashish756@gmail.com>
2024-11-15 14:34:32 +0545

    added new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import SideSection from "./components/SideSection";
 import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import noProjectImg from "./assets/no-projects.png";
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen flex gap-8">
-      <SideSection />
-      <NoProjectSelected />
-      {/* <NewProject />*/}
-      {/* <img src={noProjectImg} alt="no-projects" className="" /> */}
+      <SideSection
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/SideSection.jsx
+++ src/components/SideSection.jsx
@@ -1,14 +1,24 @@
+import { list } from "postcss";
 import Button from "./Button.jsx";
-export default function SideSection() {
+import { useState } from "react";
+export default function SideSection({ onStartAddProject, projects }) {
   return (
     <aside className="mt-5 rounded-tr-2xl bg-zinc-900 px-8 py-16 text-gray-300 md:w-72 w-1/4  ">
       <h2 className=" mb-8 md:text-xl text-3xl font-mono font-extrabold">
         YOUR PROJECTS
       </h2>
       <div>
-        <Button>+Add Projects</Button>
+        <Button onClick={onStartAddProject}>+Add Projects</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
